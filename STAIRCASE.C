122ce2cebab2d8e7a9018f5ed4729217a87d2038
Gokul Manohar <44633074+gokulmanohar@users.noreply.github.com>
2019-11-14 20:09:56 +0530

    Add files via upload

---
+++ STAIRCASE.C
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <conio.h>
+void main()
+{
+    int n, i, j, k;
+    clrscr();
+    scanf("%d",&n);
+    for(i=1;i<=n;i++)
+    {
+        for(k=1;k<=n-i;k++)
+            {
+                printf(" ");
+            }
+        for(j=1;j<=i;j++)
+            {
+                
+                printf("#");
+            }
+        printf("\n");
+    }
+    getch();
+    
+}
